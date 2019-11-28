32404f14450cc46c7d524a5dbb20315eb63bde9b
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2019-11-28 16:22:58 +0200

    added old projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './components/home/home.component';
+import { AlertComponent } from './_directives/alert.component';
+import { AlertService } from './_services/alertService.service';
+
 import { CalculatorModule } from './calculator/calculator.module';
+import { BookStoreModule } from './bookStore/bookStore.module';
+import { DrumMachineModule } from './drum-machine/drum-machine.module';
+import { RandomQuoteModule } from './random-quote/random-quote.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    CalculatorModule
+    CalculatorModule,
+    BookStoreModule,
+    DrumMachineModule,
+    RandomQuoteModule
   ],
-  providers: [],
+  providers: [
+    AlertService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
